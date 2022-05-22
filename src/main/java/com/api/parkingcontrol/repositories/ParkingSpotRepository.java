b9bce264ed4c320040db8955500f479651197df6
Pedro Abreu <pedro.schmid2311@gmail.com>
2022-05-22 01:12:12 -0300

    Created parking spot repository

---
+++ src/main/java/com/api/parkingcontrol/repositories/ParkingSpotRepository.java
@@ -0,0 +1,13 @@
+package com.api.parkingcontrol.repositories;
+
+import java.util.UUID;
+
+import com.api.parkingcontrol.models.ParkingSpotModel;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ParkingSpotRepository extends JpaRepository<ParkingSpotModel, UUID> {
+    
+} 
